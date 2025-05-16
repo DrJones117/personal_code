c008af46161c5ee152aca89408b74aba84699302
Judah Kahler <judah.kahler@gmail.com>
2025-05-16 14:09:25 -0500

    Start with llava LLM

--- AiImageRecognition/Program.cs
+++ AiImageRecognition/Program.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder();
 
+builder.Services.AddChatClient(new OllamaChatClient(new Uri("http://localhost:11434"), "llava:7b"));
+
 var app = builder.Build();
+
+var chatClient = app.Services.GetRequiredService<IChatClient>();
+
+
