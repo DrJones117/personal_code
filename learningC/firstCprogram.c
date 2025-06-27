6f9c0947df09eeba762a86a5b6aaceeaa6c1e1be
Judah Kahler <judah.kahler@gmail.com>
2025-06-27 11:23:14 -0500

    First C program!

---
+++ learningC/firstCprogram.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    
+    // This is how you comment.
+
+    /*
+    This
+    is
+    a
+    multiline
+    comment!
+    */
+
+    // This is my first program in C!
+
+    printf("Hello World!\n");
+    printf("Call of Duty used to be good.");
+
+
+    return 0;
+}
